e38ff3b9326f6332cd80a15a71375954467eece2
Sergio Cabral <scastrocabral@gmail.com>
2024-07-23 00:39:02 -0300

    add: implementa produto service

--- aula-spring/src/main/java/br/ufg/inf/espec/backend/aula_spring/service/ProdutoService.java
+++ aula-spring/src/main/java/br/ufg/inf/espec/backend/aula_spring/service/ProdutoService.java
@@ -1,5 +1,23 @@
 package br.ufg.inf.espec.backend.aula_spring.service;
 
-public class ProdutoService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.ufg.inf.espec.backend.aula_spring.model.Produto;
+import br.ufg.inf.espec.backend.aula_spring.repository.ProdutoRepository;
+
+@Service
+public class ProdutoService {
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
+	public List<Produto> listarProdutos() {
+		return produtoRepository.findAll();
+	}
+
+	public Produto salvarProduto(Produto produto) {
+		return produtoRepository.save(produto);
+	}
 }
